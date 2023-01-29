a2b9422b349e2691af773dceff0a629740b376b7
Ildar <ildaryxa@gmail.com>
2023-01-29 23:50:23 +0300

    feat: add stylelint

--- src/utils/css-transform.ts
+++ src/utils/css-transform.ts
@@ -1,9 +1,8 @@
-function parseStyle(style: any) {
+function parseStyle(style?: Record<string, string>): string {
   if (!style) {
     return '';
   }
 
-  // @ts-expect-error TS(2550): Property 'entries' does not exist on type 'ObjectC... Remove this comment to see the full error message
   return Object.entries(style)
     .map(([key, value]) => `${key}: ${value}`)
     .join(';');
